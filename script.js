1e64a442d553d2ca560c111ef6cc6dd684f9e0be
Zee Mudia <iosazee1@gmail.com>
2022-11-19 16:03:29 +0000

    added some css and modified the index.html file

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var quotes = [];
 
-function getRandomQuote() {
+function fetchQuotes() {
     fetch("https://quote-garden.herokuapp.com/api/v3/quotes")
         .then(respnse => respnse.json())
         .then(data => {
             quotes = data.data
             console.log(quotes)
-            newQuote()
+            
         })
 }
-
+    fetchQuotes();
 function newQuote() {
     var randomNumber = Math.floor(Math.random() * (quotes.length))
     document.getElementById("quote-text").innerHTML = `Quote:  ${quotes[randomNumber].quoteText}`;
